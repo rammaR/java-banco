562c4bac9e4ed827cfa3c33124da2242f0be9611
Ramar <rtawera@gmail.com>
2021-12-01 09:10:55 -0300

    Adiciona AbstractBanco. BancoDigital. CartaoDebito. Terminal. Excessoes

---
+++ src/Tempo.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Tempo {
+
+    public static void main(String[] args) throws IOException {
+        /*Scanner leitor = new Scanner(System.in);
+        while (leitor.hasNext()) {
+            double graus = leitor.nextDouble();
+
+            if (graus <= 360) {
+                if (graus < 90 || graus == 360) System.out.println("Bom Dia!!");
+                else if (        ) System.out.println("Boa Tarde!!");
+                else if (        ) System.out.println("Boa Noite!!");
+                else if (        ) System.out.println("De Madrugada!!");
+                else System.out.println("Bom Dia!!");
+
+                Double horas = Math.floor(graus / 60);
+
+                Double minutos = (  ) % 60.0;
+                Double segundos = (          ) % 60.0;
+
+                if (segundos > 59) {
+                    segundos = 0.0;
+                    minutos += 1.0;
+                }
+
+                System.out.printf("%02d:%02d:%02d%n", horas.intValue(), minutos.intValue(), segundos.intValue());
+            }
+        }*/
+    }
+
+}
